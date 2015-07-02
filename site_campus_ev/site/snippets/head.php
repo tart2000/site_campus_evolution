678dfc39d263eb2fded28e32c6ce7a66d0d8f75d
Chesnel <pa@chesnel.ca>
2015-07-02 17:12:35 -0400

    test css sass

--- site_campus_ev/site/snippets/head.php
+++ site_campus_ev/site/snippets/head.php
@@ -8,11 +8,17 @@
   <title><?php echo $site->title()->html() ?> | <?php echo $page->title()->html() ?></title>
   <meta name="description" content="<?php echo $site->description()->html() ?>">
   <meta name="keywords" content="<?php echo $site->keywords()->html() ?>">
+
+  <!-- css -->
   <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet">
   <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
   <link rel="stylesheet" href="//blueimp.github.io/Gallery/css/blueimp-gallery.min.css">
+  <link rel="stylesheet" href="assets/css/screen.css">
+
+  <!-- js  -->
   <script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
   <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+
   <link rel="icon" type="image/ico" href="/assets/images/favicon.ico" />
   <?php echo css('assets/css/main.css') ?>
 
