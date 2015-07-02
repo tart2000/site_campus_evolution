8f22e1a723db5727647d5f1877abb03be03a6284
Chesnel <pa@chesnel.ca>
2015-07-02 13:16:11 -0400

    add missing folders and create accounts

---
+++ site_campus_ev/site/accounts/arthur.php
@@ -0,0 +1,10 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: arthur
+firstname: Arthur
+lastname: Schmitt
+email: tart2000design@gmail.com
+password: >
+  $2a$10$wRQ0oX.N1z3mInytR15Pge.78dEipo.ASfyPNjhJ/7RgvT5Kp8Prm
+language: fr
+role: admin
---
+++ site_campus_ev/site/accounts/chesnel.php
@@ -0,0 +1,11 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: chesnel
+email: pa@chesnel.ca
+password: >
+  $2a$10$u8rFVyfX3TvXnSvi.IT0wehEypF6.ANU7jQjW1CUzvmw/mxfXw8cK
+language: fr
+role: admin
+token: 70f3e1e70564ad34d38894a968e8ac28afbffd80
+firstname: Pierre-Antoine
+lastname: Chesnel
