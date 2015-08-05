b94989f473faa0d8aa40a0b0853b1eed53755c9c
Arthur Schmitt <tart2000design@gmail.com>
2015-08-05 12:03:31 -0400

    new hp + home image

--- site_campus_ev/site/snippets/piliers.php
+++ site_campus_ev/site/snippets/piliers.php
@@ -5,7 +5,7 @@
 	  <?php foreach(page('offre')->children()->visible() as $key=>$pilier): ?>
 	  <?php $index++; ?>
 		<div class="col-sm-6 center pilier">
-	  		<i class="fa fa-<?php echo $pilier->icone() ?> fa-5x"></i><br>
+  			<i class="fa fa-<?php echo $pilier->icone() ?> fa-4x"></i><br>
 			<div class="title"><?php echo $index ?> - <?php echo $pilier->title() ?></div>
 			<div class="text"><?php echo $pilier->text()->excerpt(200) ?></div>
 			<a href="<?php echo $pilier->url() ?>" class="more">En savoir plus</a>
