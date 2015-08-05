96f4006783b449283ac80ac057d74e0700fe0651
Chesnel <pa@chesnel.ca>
2015-08-05 14:29:41 -0400

    header home

--- site_campus_ev/assets/js/scripts.js
+++ site_campus_ev/assets/js/scripts.js
@@ -1,13 +1,29 @@
-$(document).ready(function(){
- //http://bxslider.com/options
- $('.slider1').bxSlider({
-    // slideWidth: 400,
-    auto : true,
-    autoDelay: 5000,
-    speed : 1000,
-    minSlides: 1,
-    maxSlides: 1,
-    slideMargin: 0,
-    controls : false
-  });
+$(document).ready(function() {
+
+    //Define height banner full screen
+    resizeBanner();
+    $(window).resize(function() {
+    	resizeBanner();
+    });
+
+    //parallax effect
+      $(".banner").parallax("40%", 0.3);
+
+    function resizeBanner() {
+        $('.banner').css({
+            'height': $(window).height()-35
+        });
+    }
+
+    //http://bxslider.com/options
+    $('.slider1').bxSlider({
+        // slideWidth: 400,
+        auto: true,
+        autoDelay: 5000,
+        speed: 1000,
+        minSlides: 1,
+        maxSlides: 1,
+        slideMargin: 0,
+        controls: false
+    });
 });
